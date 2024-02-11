b366479c761382d0ea49dc2ad9ee73da3efca26f
leandrocgsi <leandrocgsi@gmail.com>
2024-02-11 14:47:59 -0300

    Adding Code to Class Library

---
+++ Erudio.HATEOAS/Hypermedia/Abstract/ISupportsHyperMedia.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+
+namespace Erudio.HATEOAS.Hypermedia.Abstract
+{
+    public interface ISupportsHyperMedia
+    {
+        List<HyperMediaLink> Links { get; set; }
+    }
+}
---
+++ Erudio.HATEOAS/Hypermedia/Filters/HyperMediaFilterOptions.cs
@@ -0,0 +1,10 @@
+﻿using Erudio.HATEOAS.Hypermedia.Abstract;
+using System.Collections.Generic;
+
+namespace Erudio.HATEOAS.Hypermedia.Filters
+{
+    public class HyperMediaFilterOptions
+    {
+        public List<IResponseEnricher> ContentResponseEnricherList { get; set; } = new List<IResponseEnricher>();
+    }
+}
