d833971f7eeeec52314c863c4c37a8ce689ff48d
Pedro Henrique Zaccaria <pedrozaccaria@gmail.com>
2024-05-25 12:35:51 -0300

    fix(sample): correct HyperMedia links endpoints
    Makes `id` parameter optional in `GetLink` function in order to have two different Href variables so the API can provide the right links.
    
    `linkWithId` is used in GET and DELETE links and `linkWithoutId` is used in POST and PUT links.

--- sample-project/RestWithASPNETErudio/RestWithASPNETErudio/Hypermedia/Enricher/PersonEnricher.cs
+++ sample-project/RestWithASPNETErudio/RestWithASPNETErudio/Hypermedia/Enricher/PersonEnricher.cs
@@ -10,40 +10,41 @@ namespace Erudio.HATEOAS.Hypermedia.Enricher
         protected override Task EnrichModel(PersonVO content, IUrlHelper urlHelper)
         {
             var path = "api/person";
-            string link = GetLink(content.Id, urlHelper, path);
+            string linkWithId = GetLink(content.Id, urlHelper, path);
+            string linkWithoutId = GetLink(null, urlHelper, path);
 
             content.Links.Add(new HyperMediaLink()
             {
                 Action = HttpActionVerb.GET,
-                Href = link,
+                Href = linkWithId,
                 Rel = RelationType.self,
                 Type = ResponseTypeFormat.DefaultGet
             });
             content.Links.Add(new HyperMediaLink()
             {
                 Action = HttpActionVerb.POST,
-                Href = link,
+                Href = linkWithoutId,
                 Rel = RelationType.self,
                 Type = ResponseTypeFormat.DefaultPost
             });
             content.Links.Add(new HyperMediaLink()
             {
                 Action = HttpActionVerb.PUT,
-                Href = link,
+                Href = linkWithoutId,
                 Rel = RelationType.self,
                 Type = ResponseTypeFormat.DefaultPut
             });
             content.Links.Add(new HyperMediaLink()
             {
                 Action = HttpActionVerb.DELETE,
-                Href = link,
+                Href = linkWithId,
                 Rel = RelationType.self,
                 Type = "int"
             });
             return Task.CompletedTask;
         }
 
-        private string GetLink(long id, IUrlHelper urlHelper, string path)
+        private string GetLink(long? id, IUrlHelper urlHelper, string path)
         {
             lock (this)
             {
